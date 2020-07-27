9b0430b55f91c36a77e96fbc31b37af863ff6256
Yuxuan Luo <yxluo@umich.edu>
2020-07-27 17:02:34 -0400

    date as generation

--- runanalyser/protowrapper/run.sh
+++ runanalyser/protowrapper/run.sh
@@ -4,3 +4,13 @@ currentDate=`date +"%Y%m%d"`
 for file in ../list/*.txt; do
   python3  main.py -k prod.apr-11-2017.pubkey -d ${file} -a asn.csv -g $currentDate
 done
+
+git clone git@github.com:refraction-networking/decoy-lists.git
+cp ../list/IR_Active.txt.blob decoy-lists/current_decoys_iran.blob
+cd decoy-lists
+git add .
+git commit -m "daily iran update $currentDate"
+git push
+
+cd ..
+rm -rf decoy-lists
